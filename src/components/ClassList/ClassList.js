7620d6e18175ff64d6ec67775e0743eeea290c20
Blake Lamb <blake.lamb31@gmail.com>
2019-06-04 15:36:03 -0600

    Finished all required and black diamond

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import Axios from 'axios'
+import {Link} from 'react-router-dom'
 
 export default class ClassList extends Component {
   constructor() {
     super()
+
+    this.state = {
+      students: []
+    }
     
+  }
+  componentDidMount() {
+    console.log(this.props)
+    Axios.get(`http://localHost:3005/students?class=${this.props.match.params.class}`).then( (res) => {
+      this.setState({
+        students: res.data
+      })
+      console.log(11111111, this.state)
+    }).catch( err => console.log(err))
   }
 
   render() {
+    let student = this.state.students.map( student => {
+    return <Link key={student.id} to={`/student/${student.id}`}><h3 >{`${student.first_name} ${student.last_name}`}</h3></Link>
+    })
     return (
       <div className="box">
-        <h1></h1>
+        <Link to='/'><h4>Back</h4></Link>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {student}
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import Axios from 'axios'
+import {Link} from 'react-router-dom'
 
 export default class Student extends Component {
   constructor() {
     super()
 
+    this.state = {
+      studentInfo : {}
+    }
+
+  }
+
+  componentDidMount() {
+    Axios.get( `http://localhost:3005/students/${this.props.match.params.id}`).then( res => {
+      this.setState({
+        studentInfo: res.data
+      })
+      console.log(this.state)
+    }).catch(err => console.log(err))
   }
 
   render() {
+    let {first_name, last_name, grade, email} = this.state.studentInfo
     return (
       <div className="box">
+        <Link to={`/classlist/${this.state.studentInfo.class}`}><h4>Back</h4></Link>
         <h1>Student</h1>
+        <h1>{`${first_name} ${last_name}`}</h1>
+        <h3>{`Grade: ${grade}`}</h3>
+        <h3>{`Email: ${email}`}</h3>
       </div>
     )
   }
